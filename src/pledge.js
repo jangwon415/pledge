5ebb740352eb211e16fb2a24f527997d61b3132d
Jang Won Ko <jangwon415@gmail.com>
2018-03-14 14:17:28 -0400

    working on ch1

--- src/pledge.js
+++ src/pledge.js
@@ -5,6 +5,33 @@ Promises Workshop: build the pledge.js ES6-style promise library
 // YOUR CODE HERE:
 
 
+class $Promise{
+  constructor(func){
+    if(typeof func !== "function") throw TypeError (/executor.+function/i);
+
+    func(this._internalResolve,this._internalReject)
+
+    this._state = "pending";
+    this._value = null;
+
+  }
+
+  _internalResolve (args){
+    if(this._state === "pending") {
+      this._state = "fulfilled";
+      this._value = args;
+    }
+
+  }
+
+  _internalReject(args){
+    if(this._state === "pending") {
+      this._value = args;
+      this._state = "rejected";
+    }
+  }
+}
+
 
 
 
