56e964464563b04737c065421293c45c8bb961e9
Jang Won Ko <jangwon415@gmail.com>
2018-03-14 18:05:58 -0400

    2 done

--- src/pledge.js
+++ src/pledge.js
@@ -7,33 +7,98 @@ Promises Workshop: build the pledge.js ES6-style promise library
 
 class $Promise{
   constructor(func){
-    if(typeof func !== "function") throw TypeError (/executor.+function/i);
-
-    func(this._internalResolve,this._internalReject)
+    if(typeof func !== "function") throw new TypeError ('executor not defined or is nto a function');
 
     this._state = "pending";
-    this._value = null;
+    this._value = undefined;
 
-  }
+    this._handlerGroups = [];
 
-  _internalResolve (args){
-    if(this._state === "pending") {
-      this._state = "fulfilled";
-      this._value = args;
+    func(this._internalResolve.bind(this), this._internalReject.bind(this));
     }
 
-  }
 
-  _internalReject(args){
-    if(this._state === "pending") {
-      this._value = args;
-      this._state = "rejected";
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+$Promise.prototype._internalResolve = function (value){
+  this._settle('fulfilled', value);
+}
+$Promise.prototype._internalReject = function (value){
+  this._settle('rejected', value);
+}
+$Promise.prototype._settle = function (state, value) {
+  if (this._state === 'pending') {
+    this._state = state;
+    this._value = value;
+    this._callHandlers();
   }
+}
+
+const isFn = val => typeof val === 'function'
+
+$Promise.prototype.then = function (successCb, errorCb) {
+  const group = {
+    successCb: isFn(successCb) ? successCb : undefined,
+    errorCb: isFn(errorCb) ? errorCb : undefined
+  }
+  this._handlerGroups.push(group);
+  this._callHandlers();
 }
 
 
 
+//   if (typeof func1 !== "function"){
+//     this.successCb = null;
+//   } else {
+//     this.successCb = func1;
+//   }
+
+
+//   if (typeof func2 !== "function"){
+//     this.errorCb = null;
+//   } else {
+//     this.errorCb = func2;
+//   }
+
+//   this._handlerGroups.push({successCb: this.successCb, errorCb: this.errorCb});
+
+//   if (this._state === "fulfilled"){
+//     this._callHandlers(this._value);
+//   }
+//   // if (this._state === "pending"){
+
+//   // }
+//   console.log(this._handlerGroups)
+
+// }
+
+
+$Promise.prototype._callHandlers = function (){
+  if (this._state !== 'pending'){
+    this._handlerGroups.forEach(group => {
+      if (group.successCb) group.successCb(this._value);
+    })
+    this._handlerGroups = [];
+  }
+
+  // this._handlerGroups[0].successCb(value);
+  // this._handlerGroups = this._handlerGroups.slice(1);
+}
 
 
 /*-------------------------------------------------------
